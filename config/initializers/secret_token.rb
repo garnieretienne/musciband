0137cc0224462acc20af863fefb5331dea4755dd
Etienne Garnier <garnier.etienne@gmail.com>
2013-10-11 21:54:44 +0200

    init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Musicband::Application.config.secret_key_base = 'c012ff3a866cfb90797c574853c26a7656839e3ba1fbe3ac4770c46dad0dbcb01698aad6eeb619421df9f10f4191a239173d35f98469b7bfc67e1fe0edaf578b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Musicband::Application.config.session_store :cookie_store, key: '_musicband_session'
